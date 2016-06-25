144109cd7b7c5b1e08b5e0d2c81b19afd009a0c9
saa <xechehot@gmail.com>
2016-06-25 14:51:15 +0500

    *Try to use FSharp.CsvProvider

--- RExp/RExp.Start/Start.fsx
+++ RExp/RExp.Start/Start.fsx
@@ -16,3 +16,22 @@ let years = wb.Countries.``Russian Federation``.Indicators.``Population, total``
 let populations = years |> Seq.map (fun y -> wb.Countries.``Russian Federation``.Indicators.``Population, total``.[y])
 R.plot(years,populations)
 R.title(main="Widgets", xlab="Period", ylab="Quantity")
+wb.Topics.Health.Description
+
+(* 1. sepal length in cm
+   2. sepal width in cm
+   3. petal length in cm
+   4. petal width in cm
+   5. class: 
+      -- Iris Setosa
+      -- Iris Versicolour
+      -- Iris Virginica *)
+type IrisClass = 
+    |Setosa
+    |Versicolour
+    |Virginica
+    
+type Iris = CsvProvider<"http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data", HasHeaders = false, Schema = "SepalLength,SepalWidth,PetalLength,PetalWidth,Name">
+let iris = Iris.Load("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data")
+let first = iris.Rows |> Seq.head
+first.Name
